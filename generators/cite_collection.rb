18db82b55209d71220ec6c7b16b7d16f58f020e7
caesarfeta <adamtavares@gmail.com>
2014-10-24 15:34:11 -0400

    Experimenting with data generator files for producing test JSON-LD files.

--- generators/cite_collection.rb
+++ generators/cite_collection.rb
@@ -1,12 +1,12 @@
-data = {
-  "title" => "title",
-  "description" => "description",
-  "urn" => "urn",
-  "name" => "name",
-  "class" => "class",
-  "namespace" => {
-    "abbr" => "abbr",
-    "full_value" => "full_value"
+@data = {
+  :title => "title",
+  :description => "description",
+  :urn => "urn",
+  :name => "name",
+  :class => "class",
+  :namespace => {
+    :abbr => "abbr",
+    :full_value => "full_value"
   },
-  "properties" => [ "property1", "property2" ]
+  :properties => [ "property1", "property2" ]
 }
