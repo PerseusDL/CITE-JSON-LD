2fefed2c9acb0449cd2901d53e8469458825bac8
caesarfeta <adamtavares@gmail.com>
2014-10-27 16:27:38 -0400

    Still working on generating realistic fake data.

--- generators/generator.rb
+++ generators/generator.rb
@@ -1,3 +1,4 @@
+
 class Generator
   # Random lower-case string n letters long
   def rstring( n )
@@ -5,12 +6,17 @@ class Generator
   end
   
   # Random integer between a and b
-  def rint( a, b )
+  def rint( a=1, b=100 )
     rand(a..b)
   end
   
   # Random float between a and b
-  def rfloat( a, b )
-    rand()*(b-a)+a
+  def rfloat( a, b, n=4 )
+    ndig( rand()*(b-a)+a, n )
   end
+  
+  # Trim float (f) to n digits
+  def ndig( f, n )
+     Float( "%.#{n}g" % f )
+   end
 end
